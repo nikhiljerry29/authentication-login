b97873b8fe06c8cc1dcad238bc5c6a97845da1fb
NIKHIL GUPTA <nikhil.gupta5667@gmail.com>
2021-01-29 23:21:37 +0530

    Add app.js listener

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+	res.redirect("/login")
+})
+
+app.get("/login", (req,res)  => {
+	res.render("login")
+})
+
+app.get("/register", (req,res)  => {
+	res.render("register")
+})
+
+
+const port = 8080;
+app.listen(8080, () => {
+	console.log("Server Started on port :: " + port)
+})
