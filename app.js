e492d4dcc87aa9e4d86bd5d633054491b015359e
NIKHIL GUPTA <nikhil.gupta5667@gmail.com>
2021-02-02 01:59:50 +0530

    add database

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
@@ -7,6 +8,17 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+const mongooseUrl = process.env.MONGOURI;
+mongoose.connect(mongooseUrl, { useNewUrlParser: true , useUnifiedTopology: true });
+
+const userSchema = new mongoose.Schema({
+	firstName: String,
+	lastName: String,
+	emailAddress: String,
+	password: String
+})
+
+const Users = new mongoose.model("user", userSchema);
 
 app.get("/", (req, res) => {
 	res.redirect("/home")
@@ -20,8 +32,33 @@ app.get("/login", (req,res)  => {
 	res.render("login")
 })
 
+app.post("/login", (req,res) => {
+	const email = req.body.emailAddress
+	const password = req.body.password
+	Users.findOne({emailAddress: req.body.emailAddress}, (err, foundUser) => {
+		if(foundUser.password === password)
+			res.render("successfulLogin",{
+				fullName: foundUser.firstName + " " + foundUser.lastName
+			})
+		else
+			res.render("unsuccessfulLogin")
+	})
+})
+
 app.get("/register", (req,res)  => {
 	res.render("register")
+})
+
+app.post("/register", (req,res) => {
+	const newUser = new Users ({
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		emailAddress: req.body.emailAddress,
+		password: req.body.password
+	})
+	newUser.save()
+
+	res.redirect('/login')
 })
 
 const port = 8080;
